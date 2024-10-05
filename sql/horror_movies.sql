d74de8d71a4e0f69c70132d624fad0f71163958a
carson-troutman <carson.troutman@gmail.com>
2024-10-04 20:04:53 -0400

    horror movie query

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,13 @@
--- Add your SQL here
+-- Take a look at the data, especially the column names
+-- SELECT * FROM movies;
+
+-- Write query that returns id, name, imdb
+-- rating of the top 3 horror movies
+-- release date >= 1985
+SELECT id as 'Movie ID', 
+name as 'Movie Title', 
+imdb_rating as 'Rating'
+FROM movies
+WHERE year <= 1985 AND genre = 'horror'
+ORDER BY Rating DESC
+Limit 3;
